f4f5246e9d47191f86af36aadd96a5d2ea97c16c
Beat Cafè i Soul <sergi@lobeat.cat>
2026-09-09 16:00:07 +0200

    Add Cloudflare adapter and prerender blog pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://sergi.page',
+  output: 'server',
+  adapter: cloudflare(),
   trailingSlash: 'ignore',
   integrations: [mdx(), sitemap()],
 });
+
