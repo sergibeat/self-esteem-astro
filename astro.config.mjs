21e979d1d4c20b1aaa6ca3a0b7eae20774aeb9c2
Beat Cafè i Soul <sergi@lobeat.cat>
2026-09-09 14:34:47 +0200

    Set site URL to sergi.page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://sergi.page',
   trailingSlash: 'ignore',
   integrations: [mdx(), sitemap()],
 });
